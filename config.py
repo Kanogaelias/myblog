d1a725203669934fca7b2b607af80b129686c8a1
Kanogaelias <kanogae@gmail.com>
2018-11-25 14:25:23 +0300

    add database uri

---
+++ config.py
@@ -0,0 +1,27 @@
+import os 
+import os.path as op
+
+class Config:
+
+    
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kanoga:mikedad@localhost/myblog'
+
+
+config_options ={"production":ProdConfig,"default":DevConfig}
+
